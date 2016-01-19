77de1a15a57aa6c2924ddd472e7a53e116dd3a08
Yoo Jae Beom <gombabo1217@naver.com>
2016-01-19 21:57:38 +0900

    add fxml class

---
+++ src/controller/ResourceController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import resource.*;
+
+/**
+ * Created by user on 2016-01-19.
+ */
+public class ResourceController {
+    @FXML
+    private TableView<Data> dataTable;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label directoryLabel;
+    @FXML
+    private Label xLabel;
+    @FXML
+    private Label yLabel;
+
+    public ResourceController(){
+
+    }
+
+
+}
